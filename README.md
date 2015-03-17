5779ddcfa79ef1e6ab1804605834e3a76931ba58
Peter Renström <renstrom.peter@gmail.com>
2015-03-17 10:56:20 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Jump Consistent Hash
+
+[![Build Status](https://travis-ci.org/renstrom/go-jump-consistent-hash.svg?branch=master)](https://travis-ci.org/renstrom/go-jump-consistent-hash)
+
+Go implementation of the jump consistent hash algorithm by John Lamping and Eric Veach[1].
+
+[1] http://arxiv.org/pdf/1406.2294v1.pdf
+
+## License
+
+MIT
---
+++ jump_test.go
@@ -0,0 +1,43 @@
+package jump
+
+import "testing"
+
+func TestHashInBucketRange(t *testing.T) {
+	h := Hash(1, 1)
+	if h != 0 {
+		t.Error("expected bucket to be 0, got", h)
+	}
+
+	h = Hash(42, 57)
+	if h != 43 {
+		t.Error("expected bucket to be 43, got", h)
+	}
+
+	h = Hash(0xDEAD10CC, 1)
+	if h != 0 {
+		t.Error("expected bucket to be 0, got", h)
+	}
+
+	h = Hash(0xDEAD10CC, 666)
+	if h != 361 {
+		t.Error("expected bucket to be 361, got", h)
+	}
+
+	h = Hash(256, 1024)
+	if h != 520 {
+		t.Error("expected bucket to be 520, got", h)
+	}
+
+}
+
+func TestNegativeBucket(t *testing.T) {
+	h := Hash(0, -10)
+	if h != 0 {
+		t.Error("expected bucket to be 0, got", h)
+	}
+
+	h = Hash(0xDEAD10CC, -666)
+	if h != 0 {
+		t.Error("expected bucket to be 0, got", h)
+	}
+}
